2b731fe6253c424fa94b86b763c17ced615ec698
Viet Anh <vietanhnguyen1050@gmail.com>
2026-08-20 00:10:21 +0700

    updated packages version, added client_url as env

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import positionRoutes from "./routes/teacherPosition.route.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ 
+  origin: process.env.CLIENT_URL || "http://localhost:5173" 
+}));
 app.use(express.json());
 
 mongoose
